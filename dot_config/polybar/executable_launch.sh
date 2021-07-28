3cd9f49c5b8c3f2e9f4f21be79272d003eabe31e
davseby <SE.David@protonmail.com>
2021-07-28 19:15:52 +0300

    Renew polybar settings

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -8,3 +8,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep; done
 
 # Launch new polybar
 polybar primary &
+
+if [ $(xrandr -q | grep " connected" | wc -l) -eq 2 ]
+then
+	polybar secondary &
+fi
