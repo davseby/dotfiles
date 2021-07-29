0f4f84003070a61284f8c5975809222f4641337e
davseby <SE.David@protonmail.com>
2021-07-29 09:43:42 +0300

    Add keyboard layout script to polybar

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Close all polybar instances
+# Close all polybar instances.
 killall -q polybar
 
-# Wait for those instances to shut down
+# Wait for those instances to shut down.
 while pgrep -u $UID -x polybar >/dev/null; do sleep; done
 
-# Launch new polybar
+# Launch main polybar.
 polybar primary &
 
+# Launch secondary polybar if second monitor is active.
 if [ $(xrandr -q | grep " connected" | wc -l) -eq 2 ]
 then
 	polybar secondary &
